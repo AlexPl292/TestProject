8b3bd2d5b4ce33f97bd2dd0317bd4e64528d16e6
Alex Plate <AlexPl292@gmail.com>
2023-06-08 11:06:25 +0300

    SVG version 1

--- src/MishaTheBest.java
+++ src/MishaTheBest.java
@@ -1,5 +1,8 @@
+import com.github.weisj.jsvg.SVGDocument;
+import com.github.weisj.jsvg.nodes.SVG;
+
 public class MishaTheBest {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println(new SVG());
     }
 }
